57cd1ac1bf7df713cdecd8494685aed17dbeaf54
Zakaria Hassan <tp19470@qmul.ac.uk>
2024-06-05 18:32:55 +0100

    Artifact Registry instead of the older Container Registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,23 @@ provider "google" {
 }
 
 # Enable the necessary APIs
+resource "google_project_service" "artifact_registry" {
+  project = var.project_id
+  service = "artifactregistry.googleapis.com"
+}
+
 resource "google_project_service" "container" {
   project = var.project_id
   service = "container.googleapis.com"
+  depends_on = [google_project_service.artifact_registry]
 }
 
-resource "google_project_service" "container_registry" {
-  project = var.project_id
-  service = "containerregistry.googleapis.com"
+# Create Artifact Registry Repository
+resource "google_artifact_registry_repository" "my_repo" {
+  provider = google
+  location = var.region
+  repository_id = "my-repo"
+  format = "DOCKER"
 }
 
 # Create GKE Cluster
@@ -22,9 +31,8 @@ resource "google_container_cluster" "primary" {
   node_config {
     machine_type = "e2-small"
     disk_size_gb = 10
-
   }
 
   initial_node_count = 1
+  depends_on = [google_project_service.container]
 }
-
